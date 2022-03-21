0cc88336e30ea11372b5110cabb6e0edecf4a679
RajeevRS11 <rs.pharma11@gmail.com>
2022-03-21 12:15:40 +0530

    Added github

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/gold_gym_3x');
-// port = 3000;
-port = process.env.PORT || 3000;
-hostname = '127.0.0.1';
+require('dotenv').config();
+
+port = process.env.PORT;
+localhost = '127.0.0.1';
 
 const express = require('express');
 const app = express();
 
-require('dotenv').config();
 
 const userRoute = require('./routes/userRoute');
 app.use('/', userRoute);
 
-app.listen(port, hostname,()=>{
-    console.log(`Server is starting at http://${hostname}:${port}/`);
+app.listen(port, localhost, ()=>{
+    console.log(`Server is starting at http://${localhost}:${port}/`);
 })
+
+// app.listen(port, ()=>{
+//     console.log(`Server is starting at http://${port}/`);
+// })
