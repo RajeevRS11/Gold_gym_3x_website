01baae916142581dc37c4ca18297547d041519a6
RajeevRS <rs.pharma11@gmail.com>
2022-03-22 14:21:22 +0530

    make it better

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/gold_gym_3x');
+mongoose.connect(process.env.DB_URL);
 require('dotenv').config();
 
 port = process.env.PORT;
-localhost = '127.0.0.1';
+// localhost = '127.0.0.1';
 
 const express = require('express');
 const app = express();
@@ -12,10 +12,10 @@ const app = express();
 const userRoute = require('./routes/userRoute');
 app.use('/', userRoute);
 
-app.listen(port, localhost, ()=>{
-    console.log(`Server is starting at http://${localhost}:${port}/`);
-})
-
-// app.listen(port, ()=>{
-//     console.log(`Server is starting at http://${port}/`);
+// app.listen(port, localhost, ()=>{
+//     console.log(`Server is starting at http://${localhost}:${port}/`);
 // })
+
+app.listen(port, ()=>{
+    console.log(`Server is starting at http://${port}/`);
+})
